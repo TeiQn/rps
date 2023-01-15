52164332b2236fde1c10af0ca74e3c6115ea9e81
teiqn <teikoresawa@gmail.com>
2023-01-15 15:26:41 +0900

    Added Scoring System, first to 5 wins.

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const buttons = document.querySelectorAll("input")
+let playerScore = 0
+let computerScore = 0
 
 function computerChoice() {
     let choices = ['Rock', 'Paper', 'Scissors']
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
 }
 
 function playRound(playerChoice){
@@ -12,13 +20,23 @@ function playRound(playerChoice){
     if ((playerChoice == "Rock" && computerSelection == "Scissors") ||
         (playerChoice == "Scissors" && computerSelection == "Paper") ||
         (playerChoice == "Paper" && computerSelection == "Rock")){
-            result = ("You win! " + playerChoice + " beats " + computerSelection)
+            playerScore += 1
+            result = ("You win! " + playerChoice + " beats " + computerSelection + "<br><br>Your Score: " + playerScore + "<br>My Score: " +computerScore)
+            if (playerScore == 5) {
+                result += "<br>You've won the Game!"
+                disableButtons()
+            }
     }
     else if (playerChoice == computerSelection){
-            result = ("It's a tie! You both chose " + playerChoice)
+            result = ("It's a tie! You both chose " + playerChoice + "<br><br>Your Score: " + playerScore + "<br>My Score: " +computerScore)
     }
     else {
-            result = ("You Lose! " + playerChoice + " loses to " + computerSelection)
+        computerScore += 1    
+        result = ("You Lose! " + playerChoice + " loses to " + computerSelection + "<br><br>Your Score: " + playerScore + "<br>My Score: " +computerScore)
+        if (computerScore == 5) {
+            result += "<br>You lost, care to try again?"
+            disableButtons()
+        }
     }
 
 document.getElementById('result').innerHTML = result
